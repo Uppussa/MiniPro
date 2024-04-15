ff99411b6150d53f69837055acc2bb4c7aacbe4d
Jose Diaz <schmuckejgd@example.com>
2024-04-15 08:05:36 -0400

    modificado

--- src/controller.js
+++
@@ -1,8 +0,0 @@
-import { pool } from './db.js'
-
-export const getUsuarios = async (req, res) => {
-  const data = await pool.query('SELECT * FROM usuarios')
-  const response = JSON.stringify(data[0])
-  res.writeHead(200, { 'Content-type': 'application/json' })
-  res.end(response)
-}
